6c81fc80d68da029036386f6e182e21a875a7694
Jane Xu <janeyx@meta.com>
2022-11-11 17:31:49 -0500

    Amended some things

--- test/test_meta.py
+++ test/test_meta.py
@@ -712,6 +712,7 @@ meta_function_device_skips = defaultdict(dict)
 
 meta_function_device_expected_failures['cpu'] = {
     torch.native_batch_norm: {bf16},
+    torch.native_batch_norm_legit: {bf16},
     torch.native_layer_norm: {bf16},
 }
 
@@ -747,6 +748,7 @@ meta_function_device_expected_failures_only_outplace['cuda'] = {
 meta_function_device_skips['cpu'] = {
     torch.narrow_copy: {b8, bf16, c128, c32, c64, f16, f32, f64, i16, i32, i64, i8, u8},
     torch.native_batch_norm: {f32, f64},
+    torch.native_batch_norm_legit: {f32, f64},
 }
 
 meta_function_device_skips['cuda'] = {
@@ -924,6 +926,7 @@ meta_dispatch_device_skips = defaultdict(dict)
 
 meta_dispatch_device_expected_failures['cpu'] = {
     aten.native_batch_norm.default: {bf16},
+    aten.native_batch_norm_legit.default: {bf16},
     aten.native_layer_norm.default: {bf16},
 }
 
@@ -969,6 +972,7 @@ meta_dispatch_device_expected_failures['cuda'] = {
 meta_dispatch_device_skips['cpu'] = {
     aten._embedding_bag_forward_only.default: {f16, f32, f64},
     aten.native_batch_norm.default: {f32, f64},
+    aten.native_batch_norm_legit.default: {f32, f64},
 }
 
 meta_dispatch_device_skips['cuda'] = {
